cc027342209c5e468c98553181a41ffc15b6b7b0
Patrick Lynch <patrick.lynch@unipart.com>
2021-06-03 16:31:08 +0100

    added localWrite function

--- local.py
+++ local.py
@@ -1,17 +1,21 @@
 from core import plugin, model
 
 class _local(plugin._plugin):
-    version = 1.0
+    version = 1.1
 
     def install(self):
         # Register models
         model.registerModel("localDelete","_localDelete","_action","plugins.local.models.action")
+        model.registerModel("localWrite","_localWrite","_action","plugins.local.models.action")
         return True
 
     def uninstall(self):
         # deregister models
         model.deregisterModel("localDelete","_localDelete","_action","plugins.local.models.action")
+        model.deregisterModel("localWrite","_localWrite","_action","plugins.local.models.action")
         return True
 
     def upgrade(self,LatestPluginVersion):
+        if self.version < 1.1:
+            model.registerModel("localWrite","_localWrite","_action","plugins.local.models.action")
         return True
--- models/action.py
+++ models/action.py
@@ -38,3 +38,49 @@ class _localDelete(action._action):
         else:
             actionResult["msg"] = "File does not exist"
         return actionResult
+
+class _localWrite(action._action):
+    localFile = str()
+    fileData = str()
+    append = bool()
+
+    class _properties(webui._properties):
+        def generate(self,classObject):
+            formData = []
+            formData.append({"type" : "break", "start" : True, "schemaitem": "Local Options"})
+            formData.append({"type" : "input", "schemaitem" : "localFile", "textbox" : classObject.localFile, "label" : "local file", "tooltip" : "The local filename (including full path) to be written"})
+            formData.append({"type" : "input", "schemaitem" : "fileData", "textbox" : classObject.fileData, "label" : "file data", "tooltip" : "The data to be written to the file"})
+            formData.append({"type" : "checkbox", "schemaitem" : "append", "textbox" : classObject.append, "label" : "append", "tooltip" : "Whether to append or overwrite the file"})
+            formData.append({"type" : "break", "start" : False, "schemaitem": "Local Options"})
+            formData.append({"type" : "break", "start" : True, "schemaitem": "Core Options"})
+            formData.append({"type" : "input", "schemaitem" : "_id", "textbox" : classObject._id})
+            formData.append({"type" : "input", "schemaitem" : "name", "textbox" : classObject.name})
+            formData.append({"type" : "checkbox", "schemaitem" : "enabled", "checked" : classObject.enabled})
+            formData.append({"type" : "json-input", "schemaitem" : "varDefinitions", "textbox" : classObject.varDefinitions})
+            formData.append({"type" : "input", "schemaitem" : "logicString", "textbox" : classObject.logicString})
+            formData.append({"type" : "checkbox", "schemaitem" : "log", "checked" : classObject.log})
+            formData.append({"type" : "input", "schemaitem" : "comment", "textbox" : classObject.comment})
+            return formData
+    
+    def run(self,data,persistentData,actionResult):
+        localFile = helpers.evalString(self.localFile,{"data" : data})
+        fileData = helpers.evalString(self.fileData,{"data" : data})
+
+        actionResult["result"] = False
+        actionResult["rc"] = 404
+        try:
+            #Fixing new lines
+            fileData = fileData.replace("\\n","\n")
+            if self.append:
+                with open(localFile,"a") as outputFile:
+                    outputFile.write(fileData)
+            else:
+                with open(localFile,"w") as outputFile:
+                    outputFile.write(fileData)
+            actionResult["msg"] = "File written to successfully"
+            actionResult["result"] = True
+            actionResult["rc"] = 0
+        except Exception as errMsg:
+            actionResult["msg"] = errMsg
+            actionResult["rc"] = 500
+        return actionResult
